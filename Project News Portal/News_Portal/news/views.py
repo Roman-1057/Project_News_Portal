23ec30c5581638a23283b7cdd847d17a88788eb3
Roman-1057 <121894492+Roman-1057@users.noreply.github.com>
2023-10-06 17:20:40 +0300

    Update views.py

--- Project News Portal/News_Portal/news/views.py
+++ Project News Portal/News_Portal/news/views.py
@@ -1,23 +1,15 @@
-# Импортируем класс, который говорит нам о том,
-# что в этом представлении мы будем выводить список объектов из БД
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import *
 
-
+# класс для представления всех постов
 class PostsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Post
-    # Поле, которое будет использоваться для сортировки объектов
     ordering = 'title'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
     template_name = 'news.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
 
-    def post_sort(request):
+    def post_sort(request):  # метод сортировки постов от новой даты к старой
         posts = Post.objects.order_by('-created_at')  # Сортировка по убыванию даты публикации
 
         context = {
@@ -27,15 +19,11 @@ class PostsList(ListView):
         return render(request, 'news.html', context)
 
 
-class PostDetail(DetailView):
-    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+class PostDetail(DetailView):  # класс отдельного поста
     model = Post
-    # Используем другой шаблон — product.html
     template_name = 'post.html'
-    # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'post'
 
-    def post_detail(request, pk):
+    def post_detail(request, pk):  # метод получения ссылки на страницу шаблона с этим постом (для перехода по ссылке на сайте)
         post = get_object_or_404(Post, pk)
-        # Ваш код для отображения деталей поста
         return render(request, 'news.html', {'post': post})
